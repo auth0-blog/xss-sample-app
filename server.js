2604fe8c66fc6124a59b38730cda09d5097f08ec
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2020-09-06 12:29:26 +0200

    Applied HTML escaping

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const session = require('express-session');
-const fs = require('fs');
 
 const port = 3000;
 const app = express();
 
 let reviews = [];
+
+app.set('views', './templates');
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 app.use(session({
@@ -13,16 +15,14 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: {
-    httpOnly: false
+    httpOnly: true
   }
 }));
 
 app.get('/', function (req, res) {
   if (req.query.newReview) reviews.push(req.query.newReview);
-  const formattedReviews = reviews.map((review)=> `<dt>User</dt><dd>${review}</dd>`).join(' ');
-  const template = fs.readFileSync('./templates/index.html', 'utf8');
-  const view = template.replace('$reviews$', formattedReviews);
-  res.send(view);
+
+	res.render('index', {reviews});
 });
 
 app.listen(port, () => console.log(`The server is listening at http://localhost:${port}`));
